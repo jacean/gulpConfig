a18e6584b2a48260341b7d12611e0c1131e85237
jacean <jaceanwan@gmail.com>
2016-07-03 16:14:36 +0800

    add app dir

---
+++ src/script/app/server.js
@@ -0,0 +1,70 @@
+var fs = require('fs'),
+    path = require('path'),
+    http = require('http');
+
+var MIME = {
+    '.css': 'text/css',
+    '.js': 'application/javascript'
+};
+
+function combineFiles(pathnames, callback) {
+    var output = [];
+
+    (function next(i, len) {
+        if (i < len) {
+            fs.readFile(pathnames[i], function (err, data) {
+                if (err) {
+                    callback(err);
+                } else {
+                    output.push(data);
+                    next(i + 1, len);
+                }
+            });
+        } else {
+            callback(null, Buffer.concat(output));
+        }
+    }(0, pathnames.length));
+}
+
+function main(argv) {
+    var config = JSON.parse(fs.readFileSync(argv[0], 'utf-8')),
+        root = config.root || '.',
+        port = config.port || 80;
+
+    http.createServer(function (request, response) {
+        var urlInfo = parseURL(root, request.url);
+
+        combineFiles(urlInfo.pathnames, function (err, data) {
+            if (err) {
+                response.writeHead(404);
+                response.end(err.message);
+            } else {
+                response.writeHead(200, {
+                    'Content-Type': urlInfo.mime
+                });
+                response.end(data);
+            }
+        });
+    }).listen(port);
+}
+
+function parseURL(root, url) {
+    var base, pathnames, parts;
+
+    if (url.indexOf('??') === -1) {
+        url = url.replace('/', '/??');
+    }
+
+    parts = url.split('??');
+    base = parts[0];
+    pathnames = parts[1].split(',').map(function (value) {
+        return path.join(root, base, value);
+    });
+
+    return {
+        mime: MIME[path.extname(pathnames[0])] || 'text/plain',
+        pathnames: pathnames
+    };
+}
+
+main(process.argv.slice(2));
